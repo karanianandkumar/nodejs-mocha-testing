108933a2fd365a001df027eca27077b127f3f94a
Anand <karanianandkumar@yahoo.com>
2018-04-16 08:56:20 +0530

    Adding expect package for assertions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,16 +1,15 @@
 const utils = require('./utils');
+const expect = require('expect');
 it('should add two numbers', () => {
     var res = utils.add(3, 6);
 
-    if (res != 9) {
-        throw new Error(`Expected 9 but got ${res}`);
-    }
-
+    expect(res).toBeA('number').toBe(9);
+    // if (res != 9) {
+    //     throw new Error(`Expected 9 but got ${res}`);
+    // }
 });
 
 it('should square a number', () => {
     var res = utils.square(5);
-    if (res != 25) {
-        throw new Error(`Should return 25 but got ${res}`)
-    }
+    expect(res).toBe(25).toBeA('number');
 })
