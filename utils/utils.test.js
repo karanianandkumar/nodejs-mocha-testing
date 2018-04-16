767daa729d71cd47cd6185b5c3a4a1e86f25736e
Anand <karanianandkumar@yahoo.com>
2018-04-16 11:46:14 +0530

    Async Function testing

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -19,6 +19,20 @@ it('should verify first and last names are set', () => {
     expect(res).toBeA('object').toInclude({ firstName: 'Anand', lastName: 'Kumar' })
 })
 
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 7, (sum) => {
+        expect(sum).toBeA('number').toBe(11);
+        done();
+    })
+})
+
+
+it('should async square the number', (done) => {
+    utils.asyncSquare(5, (square) => {
+        expect(square).toBeA('number').toBe(25);
+        done();
+    })
+})
 // it('should expect some values', () => {
     // expect(12).toNotBe(12);
     // expect({ name: 'Anand' }).toEqual({ name: 'Anand' });// For comparing Objects
