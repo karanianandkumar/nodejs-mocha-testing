01839d40c2fbdd36d7c04a46baad236fa002c463
Anand <karanianandkumar@yahoo.com>
2018-04-16 12:20:22 +0530

    Using Describe to grouping tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,34 +3,45 @@ const request = require('supertest');
 var app = require('./server').app;
 const expect = require('expect');
 
-it('should return hello world response', (done) => {
-    // request(app)
-    //     .get('/')
-    //     .expect(404)
-    //     .expect({
-    //         error: 'Page Not Found'
-    //     })
-    //     .end(done);
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page Not Found'
-            });
+describe("Server", () => {
+
+    describe("GET /", () => {
+        it('should return hello world response', (done) => {
+            // request(app)
+            //     .get('/')
+            //     .expect(404)
+            //     .expect({
+            //         error: 'Page Not Found'
+            //     })
+            //     .end(done);
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page Not Found'
+                    });
+                })
+                .end(done);
         })
-        .end(done);
-});
+
+    });
+
+    describe("GET /users", () => {
 
 
-it('should return users array', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude(
-                { name: "Anand", age: 25 }
-            )
+        it('should return users array', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude(
+                        { name: "Anand", age: 25 }
+                    )
+                })
+                .end(done);
         })
-        .end(done);
+    })
+
 })
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,38 +1,49 @@
 const utils = require('./utils');
 const expect = require('expect');
-it('should add two numbers', () => {
-    var res = utils.add(3, 6);
 
-    expect(res).toBeA('number').toBe(9);
-    // if (res != 9) {
-    //     throw new Error(`Expected 9 but got ${res}`);
-    // }
+describe('Utils', () => {
+
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(3, 6);
+
+            expect(res).toBeA('number').toBe(9);
+            // if (res != 9) {
+            //     throw new Error(`Expected 9 but got ${res}`);
+            // }
+        });
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 7, (sum) => {
+                expect(sum).toBeA('number').toBe(11);
+                done();
+            })
+        })
+
+    });
+
+    describe('#square', () => {
+
+        it('should square a number', () => {
+            var res = utils.square(5);
+            expect(res).toBe(25).toBeA('number');
+        })
+
+        it('should verify first and last names are set', () => {
+            var res = utils.setName({ age: 25 }, "Anand Kumar");
+            expect(res).toBeA('object').toInclude({ firstName: 'Anand', lastName: 'Kumar' })
+        })
+
+        it('should async square the number', (done) => {
+            utils.asyncSquare(5, (square) => {
+                expect(square).toBeA('number').toBe(25);
+                done();
+            })
+        })
+    })
+
+
 });
 
-it('should square a number', () => {
-    var res = utils.square(5);
-    expect(res).toBe(25).toBeA('number');
-})
-
-it('should verify first and last names are set', () => {
-    var res = utils.setName({ age: 25 }, "Anand Kumar");
-    expect(res).toBeA('object').toInclude({ firstName: 'Anand', lastName: 'Kumar' })
-})
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 7, (sum) => {
-        expect(sum).toBeA('number').toBe(11);
-        done();
-    })
-})
-
-
-it('should async square the number', (done) => {
-    utils.asyncSquare(5, (square) => {
-        expect(square).toBeA('number').toBe(25);
-        done();
-    })
-})
 // it('should expect some values', () => {
     // expect(12).toNotBe(12);
     // expect({ name: 'Anand' }).toEqual({ name: 'Anand' });// For comparing Objects
